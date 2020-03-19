f05f02d0ba148c1a3e226361543c192a427de56b
Martin Mendoza <marttcode@gmail.com>
2020-03-19 16:42:40 -0600

    Implements Templates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
@@ -18,4 +18,4 @@ def index():
 def hello():
     user_ip = request.cookies.get('user_ip')
 
-    return 'Hello, Marttcode. Your IP is {}'.format(user_ip)
+    return render_template('hello.html', user_ip = user_ip)
